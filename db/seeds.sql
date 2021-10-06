a0f3f5f2b16fd9592f76d82419562abba95e6511
Marco Avila <mavila6@email.arizona.com>
2021-10-06 11:04:41 -0700

    worked on seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE employeeConnection;
+
+INSERT INTO department (name);
+INSERT ("Engineer"),("Management"),("Warehouse"),("Sales");
+
+INSERT INTO role(title, salary, department_id)
+VALUES ("Lead Engineer", 100000, 1), ("Manager", 80000, 2), ("Warehouse Worker", 60000, 3), ("Sales Rep", 75000, 4);
+
+INSERT INTO Employee (first_name, last_name, role_id, manager_id)
+VALUES ("Steven", "Strange", NULL, 1), ("Marshall", "Mathers", NULL, 2), ("Jesse", "James", NULL, 3), ("Chelsea", "Power", NULL, 4);
